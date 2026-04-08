a9d9a3b268e7154f81749f116dc892ed35789d7c
Kenneth Kobina Essien Aidoo <essienkent@gmail.com>
2026-04-08 22:34:17 +0000

    working on script.js

--- script.js
+++ script.js
@@ -1,3 +1,37 @@
-body{
-    background: linear-gradient(25deg,#f181aa,#d36c76 50%);
+const minutesLabel = document.getElementById('minutes');
+const secondsLabel = document.getElementById('seconds');
+const millisecondsLabel = document.getElementById('miniseconds');
+
+const startButton = document.getElementById('startBtn');
+const stopButton = document.getElementById('stopBtn');
+const pauseButton = document.getElementById('pauseBtn');
+const resetButton = document.getElementById('resetBtn');
+
+const lapList = document.getElementById('lapList');
+
+
+///stopwatch variables
+
+let minutes = 0;
+let seconds = 0;
+let milliseconds = 0;
+let interval;
+
+startButton.addEventListener('click',startTimer);
+stopButton.addEventListener('click',stopTimer);
+pauseButton.addEventListener('click',pauseTimer);
+resetButton.addEventListener('click',resetTimer);
+
+
+function startTimer(){
+
+}
+function stopTimer(){
+
+}
+function pauseTimer(){
+
+}
+function resetTimer(){
+    
 }
