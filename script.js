58adc3e250e046946709f1b0d4f6a7dc62aa1d2e
Kenneth Kobina Essien Aidoo <essienkent@gmail.com>
2026-04-11 22:06:57 +0000

    overload and distortion

--- script.js
+++ script.js
@@ -1,16 +1,15 @@
 const minutesLabel = document.getElementById('minutes');
 const secondsLabel = document.getElementById('seconds');
-const millisecondsLabel = document.getElementById('miniseconds');
+const millisecondsLabel = document.getElementById('milliseconds');
 
 const startButton = document.getElementById('startBtn');
 const stopButton = document.getElementById('stopBtn');
 const pauseButton = document.getElementById('pauseBtn');
 const resetButton = document.getElementById('resetBtn');
 
-const lapList = document.getElementById('lapList');
+const lapList = document.getElementById('laplist');
 
-
-///stopwatch variables
+/// stopwatch variables
 
 let minutes = 0;
 let seconds = 0;
@@ -25,35 +24,34 @@ resetButton.addEventListener('click',resetTimer);
 
 function startTimer(){
 
-    interval = setInterval(updateTimer,10);
-    startButton.disabled= true;
-
-  
+    interval =  setInterval(updateTimer,10);
+    startButton.disabled = true;
 
 }
+
 function stopTimer(){
 
     clearInterval(interval);
     addToLapList();
     resetTimerData();
     startButton.disabled = false;
-
 }
+
 function pauseTimer(){
     clearInterval(interval);
-    pauseButton.disabled= false;
-
+    startButton.disabled = false;
 }
+
 function resetTimer(){
     clearInterval(interval);
     resetTimerData();
     startButton.disabled = false;
 
-
 }
+
 function updateTimer(){
-    milliseconds++
-    if(milliseconds === 100){ ////1000 1seconds= 1000millisecondds
+    milliseconds++;
+    if(milliseconds === 100){  //// 1000  -> 1 seconds = 1000 millseconds
         milliseconds = 0;
         seconds++;
         if(seconds === 60){
@@ -67,24 +65,25 @@ function updateTimer(){
 
 function displayTimer(){
     millisecondsLabel.textContent = padTime(milliseconds);
-    secondsLabel.textContent = padTimer(seconds);
-    minutesLabel.textContent = padTime(minutes);
+    secondsLabel.textContent = padTime(seconds);
+    minutesLabel.textContent = padTime(minutes);    
 }
 
-function padTimer(time){
+function padTime(time){
     return time.toString().padStart(2,'0');
-
 }
+
 function resetTimerData(){
     minutes = 0;
     seconds = 0;
-    miliseconds = 0;
+    milliseconds = 0;
     displayTimer();
 }
+
 function addToLapList(){
-    const lapTime = ${padTime(mintues)}; ${padTime(seconds)}:${padTime(milliseconds)};
+    const lapTime = `${padTime(minutes)}:${padTime(seconds)}:${padTime(milliseconds)}`;
+
     const listItem = document.createElement('li');
-    listitem.innnerHTML = <span>Lap ${lapList.childElementCount + 1}: </span>$
-    {lapTime};
-    lapList.appendChild(listItem)
+    listItem.innerHTML = `<span>Lap ${lapList.childElementCount + 1}: </span>${lapTime}`;
+    lapList.appendChild(listItem);
 }
